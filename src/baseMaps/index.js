36a1e7838d4d7aa68b1baaaab1d638cf2bf1148e
DVGY <gaurav.gydv@gmail.com>
2023-06-11 22:14:40 +0530

    add custom tile layer and set zoom level to 6

--- src/baseMaps/index.js
+++ src/baseMaps/index.js
@@ -1,18 +1,17 @@
 export const mapBox = L.tileLayer.provider('MapBox', {
   attribution:
     'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
+  maxZoom: 5,
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1,
-  accessToken:
-    'pk.eyJ1IjoiZHZneSIsImEiOiJja2tiZHcwZDEwNWZvMzFsY3A0NXNiNjh0In0.3LKeQ0CqVP3tWGmAf3bcDA',
+  accessToken: 'your api key',
 });
 
 export const OpenTopo = L.tileLayer(
   'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
   {
-    maxZoom: 17,
+    maxZoom: 5,
     attribution:
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
   }
@@ -21,13 +20,18 @@ export const OpenTopo = L.tileLayer(
 export const ESRIImagery = L.tileLayer(
   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   {
+    maxZoom: 5,
     attribution:
       'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
   }
 );
 
-export const customTileLayer = L.tileLayer('/js/tiles/tiles_{z}_{x}_{y}.jpg', {
-  maxZoom: 5,
-  noWrap: true,
-  attribution: 'Some Attribution',
-});
+export const customTileLayer = L.tileLayer(
+  'http://localhost:1370/tiles/{z}/{x}/{y}.jpg',
+  {
+    maxZoom: 6,
+    noWrap: true,
+    // attribution:
+    //   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
 import './style.css';
 import { mapInstance } from './mapInstance';
-import { mapBox, ESRIImagery, OpenTopo } from './baseMaps';
+import { mapBox, ESRIImagery, OpenTopo, customTileLayer } from './baseMaps';
+
+const ZoomViewer = L.Control.extend({
+  onAdd() {
+    const gauge = L.DomUtil.create('div');
+    gauge.style.width = '200px';
+    gauge.style.background = 'rgba(255,255,255,0.5)';
+    gauge.style.textAlign = 'left';
+    mapInstance.on('zoomstart zoom zoomend', (ev) => {
+      gauge.innerHTML = `Zoom level: ${mapInstance.getZoom()}`;
+    });
+    return gauge;
+  },
+});
+
+new ZoomViewer().addTo(mapInstance);
 
 L.control
   .layers({
-    'Map Box': mapBox,
-    'ESRI World Imagery': ESRIImagery,
-    'Open Topo': OpenTopo,
+    // 'Map Box': mapBox,
+    // 'ESRI World Imagery': ESRIImagery,
+    // 'Open Topo': OpenTopo,
+    'Custom Image': customTileLayer,
   })
   .addTo(mapInstance);
 
-mapBox.addTo(mapInstance);
+customTileLayer.addTo(mapInstance);
